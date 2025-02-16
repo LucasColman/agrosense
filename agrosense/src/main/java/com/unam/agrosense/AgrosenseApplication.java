62ffdbf21aefb0dfd668943806b27eec488f545d
Lucas Colman <lukasscolman@gmail.com>
2025-02-15 21:17:29 -0300

    carga de proyecto

---
+++ agrosense/src/main/java/com/unam/agrosense/AgrosenseApplication.java
@@ -0,0 +1,13 @@
+package com.unam.agrosense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosenseApplication.class, args);
+	}
+
+}
