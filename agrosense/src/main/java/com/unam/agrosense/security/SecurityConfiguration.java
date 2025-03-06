0199f74e4a1e8be0b0ae5d870a5606f66366013b
Lucas Colman <lukasscolman@gmail.com>
2025-03-06 19:37:46 -0300

    retrospectiva iteracion 1

--- agrosense/src/main/java/com/unam/agrosense/security/SecurityConfiguration.java
+++ agrosense/src/main/java/com/unam/agrosense/security/SecurityConfiguration.java
@@ -1,16 +1,14 @@
 package com.unam.agrosense.security;
 
 
-import com.unam.agrosense.model.usuario.Rol;
-import com.unam.agrosense.model.usuario.Usuario;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,55 +27,28 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(req -> {
-
-                    req.anyRequest().permitAll();
-
-//                    req.requestMatchers("/login","/signup","/dashboard", "/css/**", "/js/**").permitAll();
-//
-//                    //permitir acceso a estos endpoints sin autenticación
-//                    req.requestMatchers(HttpMethod.POST, "/auth","/usuarios/registro").permitAll();
-//
-//
-//                    req.requestMatchers(HttpMethod.GET,
-//                            "/sensores/**",
-//                            "/actuadores",
-//                            "/tipos-sensores/**",
-//                            "/tipos-actuadores/**",
-//                            "/usuarios/**").permitAll();
-//
-//                    req.requestMatchers(HttpMethod.POST,
-//                            "/sensores/**",
-//                            "/actuadores/**",
-//                            "/tipos-sensores/**",
-//                            "/tipos-actuadores/**").permitAll();
-//
-//                    req.requestMatchers(HttpMethod.PUT,
-//                            "/sensores/**",
-//                            "/actuadores/**",
-//                            "/tipos-sensores/**",
-//                            "/tipos-actuadores/**").permitAll();
-//
-//                    req.requestMatchers(HttpMethod.DELETE,
-//                            "/sensores/**",
-//                            "/actuadores/**",
-//                            "/tipos-sensores/delete/**",
-//                            "/tipos-actuadores/**",
-//                            "/usuarios/**").permitAll();
+        http
+                .cors(Customizer.withDefaults()) // Habilita CORS
+                .csrf(csrf -> csrf.disable()) // Deshabilita CSRF si usas JWT
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login","/signup", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/auth","/usuarios/registro").permitAll()
+                        .requestMatchers("/dashboard").hasRole("ADMIN")
+                        .requestMatchers("/").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT no usa sesiones
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
 
-                })
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        return http.build();
     }
-
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-
     }
 
     @Bean
@@ -85,3 +56,4 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 }
+
---
+++ agrosense/src/main/resources/static/js/auth.js
@@ -0,0 +1,35 @@
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    try {
+        const response = await fetch('/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+        if (!response.ok) {
+            alert('Credenciales inválidas');
+            return;
+        }
+        const data = await response.json();
+        // Guardar el token
+        localStorage.setItem('authToken', data.token);
+        localStorage.setItem('role', data.rol);
+        // Redirigir según el rol
+        if (data.rol === "ADMIN") {
+            window.location.href = "/dashboard";
+            await cargarDatos();
+        } else {
+            window.location.href = "/";
+            await cargarDatos();
+        }
+    } catch (error) {
+        console.error("Error durante el auth", error);
+        alert('Error durante el auth');
+    }
+}
