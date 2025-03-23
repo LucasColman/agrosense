7c4df0fcfd89f500651dbc69c79c307f5697bf74
Lucas Colman <lukasscolman@gmail.com>
2025-03-23 15:19:41 -0300

    vista de sensores modificada

--- agrosense/src/main/resources/static/js/dispositivos/sensores.js
+++ agrosense/src/main/resources/static/js/dispositivos/sensores.js
@@ -79,6 +79,31 @@ async function obtenerSensores() {
     }
 }
 
+// Función para buscar en la tabla de actuadores
+document.addEventListener('DOMContentLoaded', function () {
+    // Función de búsqueda en la tabla
+    document.getElementById('searchInput').addEventListener('keyup', function () {
+        let searchText = this.value.toLowerCase();
+        let table = document.getElementById('sensoresTable');
+        let rows = table.getElementsByTagName('tr');
+
+        for (let i = 1; i < rows.length; i++) {
+            let found = false;
+            let cells = rows[i].getElementsByTagName('td');
+
+            for (let j = 0; j < cells.length - 1; j++) {
+                let cellText = cells[j].innerText.toLowerCase();
+                if (cellText.indexOf(searchText) > -1) {
+                    found = true;
+                    break;
+                }
+            }
+
+            rows[i].style.display = found ? '' : 'none';
+        }
+    });
+});
+
 
 
 
